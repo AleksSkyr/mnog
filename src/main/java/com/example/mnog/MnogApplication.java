40ef30691d795ae0e576e333f1378a6c8df51be1
AleksSkyr <greshnik1499@gmail.com>
2023-07-31 00:45:00 +0300

    st

---
+++ src/main/java/com/example/mnog/MnogApplication.java
@@ -0,0 +1,13 @@
+package com.example.mnog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MnogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MnogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mnog/MnogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mnog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MnogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
